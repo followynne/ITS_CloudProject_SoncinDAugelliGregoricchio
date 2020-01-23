b52798ce832d453e11166aa8ce6ec3dc67769986
Erika Soncin <e.soncin@studenti.local>
2020-01-23 12:09:39 +0100

    merge_completed

---
+++ templates/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $this->e($title) ?></title>
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+    
+    <?= $this->section('content')?>
+    <?= $this->insert('footer') ?>
+
+    <link rel="icon" type="image/png" href="/images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href= "/node_modules/animate.css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="/node_modules/hamburgers/dist/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="/node_modules/select2/dist/css/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="/css/util.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/css/footer.css">
+    <!-- <link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css"> -->   
+    <!-- <link rel="stylesheet" type="text/css" href="/vendor/daterangepicker/daterangepicker.css"> -->
+</body>
+</html>
